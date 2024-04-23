fc2c87a44f2ebcc466eb542ec105e26ea43fc2b6
Olena Kryvenko <bona240609@gmail.com>
2024-04-23 11:41:26 +0200

    is done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Contact from "../Contact/Contact.jsx";
-import './ContactList.css'
+import "./ContactList.css";
 
-const ContactList = ({ contacts, onDelete }) => {
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const nameFilter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Contacts</h2>
       <ul className="contacts-list">
-        {contacts.map((contact) => (
-          <Contact key={contact.id} contact={contact} onDelete={onDelete} />
+        {filteredContacts.map(({ id, name, number }) => (
+          <Contact key={id} name={name} id={id} number={number} />
         ))}
       </ul>
     </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+// import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
@@ -17,6 +27,13 @@ const store = configureStore({
     contacts: persistedContactsReducer,
     filters: filtersReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
